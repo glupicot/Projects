3d8c24685e1fb9d3bfd0bdd735989b62b2955016
Lyubov <luartal@mail.ru>
2025-06-18 22:44:24 +0300

    ahyetb

---
+++ todo/glaz.js
@@ -0,0 +1,40 @@
+document.addEventListener("DOMContentLoaded", () => {
+    let eyeicon = document.getElementById("myImage");
+    let password = document.getElementById("myPass");
+    let eyeicon2 = document.getElementById("myImage2");
+    let password2 = document.getElementById("myPass2");
+
+    eyeicon.onclick = function() {
+    if(password.type =="password"){
+        password.type = "text";
+        eyeicon.src = "../todo/assets/icons/glazok.svg";
+    }
+    else {
+        password.type = "password";
+        eyeicon.src = "../todo/assets/icons/glazok-closed.svg";
+    }
+    }
+
+    eyeicon2.onclick = function() {
+    if(password2.type =="password"){
+        password2.type = "text";
+        eyeicon2.src = "../todo/assets/icons/glazok.svg";
+    }
+    else {
+        password2.type = "password";
+        eyeicon2.src = "../todo/assets/icons/glazok-closed.svg";
+    }
+    }
+})
+
+
+  const input = document.getElementById('myInput');
+  const button = document.getElementById('myButton');
+
+  input.addEventListener('input', function() {
+    if (input.value.trim() !== '') {
+      button.style.backgroundColor = 'green';
+    } else {
+      button.style.backgroundColor = 'red';
+    }
+  });
