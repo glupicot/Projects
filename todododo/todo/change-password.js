409eddfe1fa04a35efb5c48ca61359f8a898a8a7
Lyubov <luartal@mail.ru>
2025-09-22 14:38:40 +0300

    цукенгш

--- todododo/todo/change-password.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const input = document.getElementById('myInput');
-    const button = document.getElementById('myButton');
-    input.addEventListener('input', function() {
-        const hasText = this.value.trim() !== '';
-        button.classList.toggle('active-button', hasText);
-    });
-});
